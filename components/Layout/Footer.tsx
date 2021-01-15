8f28d9503efa2f596d210e6048744bed9dcffafe
Daniel Ohn <ohndaniel@gmail.com>
2021-01-15 13:52:23 -0700

    Add Social links to footer.

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,6 +1,14 @@
+import Twitter from "../Icons/Twitter"
+import Instagram from "../Icons/Instagram"
+import Tiktok from "../Icons/Tiktok"
+
 function Footer() {
   return (
-    <footer className="m-auto w-full h-1/6 border border-red-500">feet</footer>
+    <footer className="m-auto w-full h-1/6 flex flex-nowrap flex-row justify-center border border-red-500">
+      <Twitter className="p-4" />
+      <Instagram className="p-4" />
+      <Tiktok className="p-4" />
+    </footer>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-blue-400">Testing</h1>
+      <h1 className="text-blue-400">welcome</h1>
     </>
   )
 }
