04d816f4a72f1298e12243f38e847dbf5ae6a20d
Daniel Ohn <ohndaniel@gmail.com>
2021-01-15 13:59:10 -0700

    Add flex-end to each social link

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -5,9 +5,9 @@ import Tiktok from "../Icons/Tiktok"
 function Footer() {
   return (
     <footer className="m-auto w-full h-1/6 flex flex-nowrap flex-row justify-center border border-red-500">
-      <Twitter className="p-4" />
-      <Instagram className="p-4" />
-      <Tiktok className="p-4" />
+      <Twitter className="p-4 self-end" />
+      <Instagram className="p-4 self-end" />
+      <Tiktok className="p-4 self-end" />
     </footer>
   )
 }
